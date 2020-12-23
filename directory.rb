bfd6ed02bd52e0173495b8ade73ca4b25563691a
jshields123 <jackshields@live.co.uk>
2020-12-23 13:47:33 +0000

    exercise 8 Q2

--- directory.rb
+++ directory.rb
@@ -18,33 +18,28 @@ def input_students
 end
 
 
-# let's put all students into an array
-# students = [
-#   {name: "Dr. Hannibal", cohort: :november},
-#   {name: "Darth Vader", cohort: :november},
-#   {name: "Nurse Ratched", cohort: :november},
-#   {name: "Michael Corleone", cohort: :november},
-#   {name: "Alex DeLarge", cohort: :november},
-#   {name: "The Wicked Witch of the West", cohort: :november},
-#   {name: "Terminator", cohort: :november},
-#   {name: "Freddy Krueger", cohort: :november},
-#   {name: "The Joker", cohort: :november},
-#   {name: "Joffrey Baratheon", cohort: :november},
-#   {name: "Norman Bates", cohort: :november}
-# ]
+
 def print_header
   puts "The students of Villians Academy"
   puts "-------------"
 end
 
+
+
 def print(students)
-  students.each do |student|
+  students.select do |student|
+  student.each do |k, v|
+    if v[0] == "J"
   puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    end
   end
 end
+end
+
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
+
 # nothing happens until we call the methods.
 students = input_students
 print_header
