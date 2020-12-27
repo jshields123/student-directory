b374d541e9a16b64abc7a216ccf1e87356f64710
jshields123 <jackshields@live.co.uk>
2020-12-27 16:48:56 +0000

    exercise 8 Q5

--- directory.rb
+++ directory.rb
@@ -5,44 +5,55 @@ def input_students
   students = []
   # get the first names
   name = gets.chomp
+
+  puts "Please enter the country of birth"
+  country_of_birth = gets.chomp
+
+  puts "Please enter your height in cm"
+  height = gets.chomp
+
   # while the name is not empty, repeat this code
-  while !name.empty? do
+  while  !name.empty? # && !height.empty? && !country_of_birth.empty?
     # add the student hash to the array
-    students << {name: name, cohort: :november}
+    students << {name: name, height: height, country_of_birth: country_of_birth, cohort: :november}
+
     puts "Now we have #{students.count} students"
+
+    puts "Please enter the names of the students"
     # get another name from the user
     name = gets.chomp
+
+    if name.empty?
+      puts "breaking"
+      break
+    else
+    puts "Please enter the country of birth"
+    country_of_birth = gets.chomp
+
+    puts "Please enter your height in cm"
+    height = gets.chomp
   end
+end
   # return the array of students
 students
 end
 
 
-
-#
 def print_header
   puts "The students of Villians Academy"
   puts "-------------"
 end
-#
+
 def print(students)
-  count = 0
+  count = 1
     until count >= students.count
       students.each_with_index do |student, index|
-      puts "#{student[:name]} (#{student[:cohort]} cohort)"
+      puts "#{index + 1}. #{student[:name]}, height: #{student[:height]}cm, country of birth: #{student[:country_of_birth]} (#{student[:cohort]} cohort)"
     end
       count += 1
 
   end
 end
-# def print(students)
-#   students.each do |student|
-#     if student[:name].length < 12
-#   puts "#{student[:name]} (#{student[:cohort]} cohort)"
-#     end
-#   end
-# end
-
 
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
