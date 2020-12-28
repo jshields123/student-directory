5cccb5b3a8e949f155ba5407660260922ddf144d
jshields123 <jackshields@live.co.uk>
2020-12-28 19:17:31 +0000

    exercise 8 Q7

--- directory.rb
+++ directory.rb
@@ -1,10 +1,13 @@
 def input_students
   puts "Please enter the names of the students".center(150)
-  puts "To finish, just hit enter twice".center(150)
+  puts "To exit, just enter 'exit'".center(150)
   # create an empty array
-  students = []
-  # get the first names
+  @students = []
+
   name = gets.chomp
+
+  puts "Please enter the students cohort".center(150)
+  cohort = gets.chomp
 
   puts "Please enter the country of birth".center(150)
   country_of_birth = gets.chomp
@@ -13,29 +16,34 @@ def input_students
   height = gets.chomp
 
   # while the name is not empty, repeat this code
-  while  !name.empty? # && !height.empty? && !country_of_birth.empty?
+  while true do # && !height.empty? && !country_of_birth.empty?
     # add the student hash to the array
-    students << {name: name, height: height, country_of_birth: country_of_birth, cohort: :november}
+    @students << {name: name.capitalize, height: height, country_of_birth: country_of_birth, cohort: cohort}
 
-    puts "Now we have #{students.count} students".center(150)
+    puts "Now we have #{@students.count} students".center(150)
 
     puts "Please enter the names of the students".center(150)
-    # get another name from the user
     name = gets.chomp
+# put in a if the is exit = break the loop.
+    if name == 'exit'
+    exit
 
-    if name.empty?
-      break
-    else
+
+    puts "Please enter the students cohort".center(150)
+    cohort = gets.chomp
+
     puts "Please enter the country of birth".center(150)
     country_of_birth = gets.chomp
 
     puts "Please enter your height in cm".center(150)
     height = gets.chomp
-  end
 end
-  # return the array of students
-students
+@students
 end
+end
+
+
+
 
 
 def print_header
@@ -44,21 +52,22 @@ def print_header
 end
 
 def print(students)
-  count = 0
-    until count >= students.count
       students.each_with_index do |student, index|
-      puts "#{index + 1}. #{student[:name]}, height: #{student[:height]}cm, country of birth: #{student[:country_of_birth]} (#{student[:cohort]} cohort)".center(150)
-count += 1
+      puts "#{index + 1}. #{student[:name]}, Height: #{student[:height]}cm, Country of birth: #{student[:country_of_birth]} (#{student[:cohort]} cohort)".center(150)
     end
+  end
+
+def print_footer(names)
+  if @students.length == 1
+    puts "Overall, we have #{names.count} great student".center(150)
+  elsif @students.length < 1
+    puts "Overall, we have #{names.count} great students!".center(150)
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students".center(150)
-end
 
 # nothing happens until we call the methods.
-students = input_students
+@students = input_students
 print_header
-print(students)
-print_footer(students)
+print(@students)
+print_footer(@students)
